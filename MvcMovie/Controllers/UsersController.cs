cc424025c4efa827e213f04ccac3957bcaf35885
MariusTBjercke <marius@bjerckemedia.no>
2022-02-14 19:22:51 +0100

    Add project files.

---
+++ MvcMovie/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.SqlClient;
+using System.Text.Encodings.Web;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> List(string name, int numTimes = 1)
+        {
+            SqlReader reader =
+                new SqlReader(
+                    @"Data Source=DESKTOP-LDL14VM\SQLEXPRESS;Initial Catalog=MYTESTSQL;Integrated Security=True");
+
+            ViewData["Users"] = await reader.GetUsers();
+
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Models/SqlReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using Dapper;
+
+namespace MvcMovie.Models
+{
+    public class SqlReader
+    {
+        private string _connectionString { get; set; }
+
+        public SqlReader(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<List<Person>> GetUsers()
+        {
+            List<Person> people = new();
+            var query = $"select * from Users";
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                people = connection.Query<Person>(query).ToList();
+            }
+
+            return people;
+        }
+    }
+}
